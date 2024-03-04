b497851102a0c6fd5776d401e435109fddc18b74
thig0w <tw@weidman.com.br>
2024-03-04 17:51:32 -0300

    Fixing default year to the current year

--- retailcalendar/cli.py
+++ retailcalendar/cli.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import date
+
 import click
 
 from retailcalendar import Cal454
@@ -6,7 +8,7 @@ from retailcalendar import Cal454
 
 @click.command()
 @click.option("-s", "--start_month", type=int, default=1)
-@click.argument("year", type=int, default=2023)
+@click.argument("year", type=int, default=date.today().year)
 def getcalendar(year, start_month):
     c = Cal454(year, s_month=start_month)
     c.format_year()
